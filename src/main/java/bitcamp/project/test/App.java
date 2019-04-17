c728e9895897414bc66c2d29d8efda5d9a172e82
SeungWanWoo <seungwan.woo94@gmail.com>
2019-04-17 16:10:46 +0900

    파일서비스 테스트
    파일서비스를 테스트한다.

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -3,7 +3,6 @@ package bitcamp.project.test;
 import bitcamp.project.test.service.FileService;
 import bitcamp.project.test.service.BoardService;
 import bitcamp.project.test.service.ManagerService;
->>>>>>> branch 'master' of https://github.com/eikhyeonchoi/bitcamp-project-test.git
 import bitcamp.project.test.service.MemberService;
 
 public class App {
@@ -12,14 +11,11 @@ public class App {
     }
     
 
-<<<<<<< HEAD
   public static void main(String[] args) {
     System.out.println("*************************************");
     FileService fs = new FileService();
     fs.list();
     System.out.println("*************************************");
-=======
-    public static void main(String[] args) {
       System.out.println("gg");
       BoardService boardService = new BoardService();
       boardService.list();
@@ -28,7 +24,6 @@ public class App {
     s.list();
     System.out.println("--------------------");
     
->>>>>>> branch 'master' of https://github.com/eikhyeonchoi/bitcamp-project-test.git
     MemberService m = new MemberService();
     
     for (int i=0; i < 10; i++) {
